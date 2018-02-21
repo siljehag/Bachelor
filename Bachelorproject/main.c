4583e4838bbf71a9f5e6622e9c2a939b3d3c9f81
siljehag <silje.095@hotmail.com>
2018-02-21 12:25:35 +0100

    Added AVR project

---
+++ Bachelorproject/main.c
@@ -0,0 +1,18 @@
+/*
+ * Bachelorproject.c
+ *
+ * Created: 21.02.2018 12.13.22
+ * Author : uioit
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
